30825e00e601fe05cb883d50fbf7686ad81428cb
engelsa <engelsa@purdue.edu>
2020-01-25 16:34:22 -0500

    beautified the UI

--- interface/script.js
+++ interface/script.js
@@ -1,1 +1,10 @@
+let fileNameLabel = document.getElementById("filename");
+let fileInput = document.getElementById("file");
 
+function updateFileNameLabel(e) {
+	if (e.target.files.length > 0) {
+		fileNameLabel.innerHTML = e.target.files[0].name;
+	}
+}
+
+fileInput.addEventListener("input", updateFileNameLabel);
