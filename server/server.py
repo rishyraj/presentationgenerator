1fbc535d7a488a8d36cff731f5630547058b3323
lu656 <lu656@purdue.edu>
2020-01-25 16:49:13 -0500

    speech to text recognition added

--- server/server.py
+++ server/server.py
@@ -1,9 +1,17 @@
 import os
+import io
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
 from flask import Flask, render_template, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
+# Instantiate google cloud speech client
+client = speech.SpeechClient()
+
+
 UPLOAD_FOLDER = './temp_storage/'
-ALLOWED_EXTENSIONS = {'txt', 'pdf'}
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'mp3'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -22,9 +30,31 @@ def upload_file():
       extension = f.filename.split('.')[-1]
       if (extension == 'txt'):
         f.save(UPLOAD_FOLDER + f.filename)
-      # elif (extension == 'pdf'):
-      
-      # elif ()
+      elif (extension == 'pdf'):
+        print("hi")
+      elif (extension == 'mp3'):
+        f.save(UPLOAD_FOLDER + f.filename)
+        audio_name = UPLOAD_FOLDER + f.filename
+        # Load audio to memory
+        with io.open(audio_name, 'rb') as audio_file:
+          content = audio_file.read()
+          audio = types.RecognitionAudio(content=content)
+
+        config = types.RecognitionConfig(
+          encoding=enums.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED,
+          sample_rate_hertz=16000,
+          language_code='en-US',
+          enable_automatic_punctuation=True
+        )
+
+        # Detects speech in the audio file
+        response = client.recognize(config, audio)
+        f = open(UPLOAD_FOLDER + f.filename.split('.')[0] + '.txt', 'w')
+        for result in response.results:
+          # print('Transcript: {}'.format(result.alternatives[0].transcript))
+          f.write(result.alternatives[0].transcript + '\n')
+        f.close()
+        
       return redirect('/')
     else:
       return 'invalid file extension'
