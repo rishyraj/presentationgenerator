08597929f0d1ce502a46dfe05f74ced8880a6faa
lu656 <lu656@purdue.edu>
2020-01-25 18:08:15 -0500

    text area works

--- server/server.py
+++ server/server.py
@@ -1,10 +1,21 @@
+from __future__ import print_function
+import pickle
+import os.path
 import os
 import io
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 from flask import Flask, render_template, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+
+
+SCOPES = ['https://www.googleapis.com/auth/presentations.readonly']
+PRESENTATION_ID = '1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc'
+
 
 # Instantiate google cloud speech client
 client = speech.SpeechClient()
@@ -19,14 +30,24 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')
 def index():        # render the html to page
   print('in the index function')
-  return render_template('index.html')
+  return render_template('page.html')
   # return "Hello World"
 
 @app.route('/', methods=['POST'])   # 
 def upload_file():
+  creds = None
   if request.method == 'POST':
-    f = request.files['file']
-    if (allowed_file(f.filename)):
+    try:
+      f = request.files['file']
+    except:
+      f = request.form['rawtext']
+      print(type(f))
+    if (isinstance(f,str)):
+      textfile = open(UPLOAD_FOLDER + 'raw_text_file.txt', 'w');
+      textfile.write(f);
+      textfile.close()
+      return redirect('/')
+    elif (allowed_file(f.filename)):
       extension = f.filename.split('.')[-1]
       if (extension == 'txt'):
         f.save(UPLOAD_FOLDER + f.filename)
@@ -49,12 +70,32 @@ def upload_file():
 
         # Detects speech in the audio file
         response = client.recognize(config, audio)
-        f = open(UPLOAD_FOLDER + f.filename.split('.')[0] + '.txt', 'w')
+        file = open(UPLOAD_FOLDER + f.filename.split('.')[0] + '.txt', 'w')
         for result in response.results:
           # print('Transcript: {}'.format(result.alternatives[0].transcript))
-          f.write(result.alternatives[0].transcript + '\n')
-        f.close()
+          file.write(result.alternatives[0].transcript + '\n')
+        file.close()
         
+      # if os.path.exists('token.pickle'):
+      #   with open('token.pickle', 'rb') as token:
+      #     creds = pickle.load(token)
+      # if not creds or not creds.valid:
+      #   if creds and creds.expired and creds.refresh_token:
+      #     creds.refresh(Request())
+      #   else:
+      #     flow = InstalledAppFlow.from_client_secrets_file(
+      #       'credentials.json', SCOPES
+      #     )
+      #     creds = flow.run_local_server(port=0)
+      #   with open('token.pickle', 'wb') as token:
+      #     pickle.dump(creds, token)
+        
+      #   service = build('slides', 'v1', credentials=creds)
+
+      #   presentation = service.presentations().get(
+      #     presentationId=PRESENTATION_ID
+      #   ).set_execute()
+      #   slides = presentation.get('slides')
       return redirect('/')
     else:
       return 'invalid file extension'
